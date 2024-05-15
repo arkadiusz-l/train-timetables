148d605457e1df034533c4d0c9ddc288e15c5eea
arkadiusz-l <104087320+arkadiusz-l@users.noreply.github.com>
2024-05-15 12:16:52 +0200

    Add configuration loading from YAML file

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import os
 import re
 from time import sleep
 import requests
+import yaml
 from bs4 import BeautifulSoup
 from pypdf import PdfReader
 from tqdm import tqdm
@@ -17,17 +18,17 @@ class LinkNotFound(Exception):
     pass
 
 
-def parse_pdf_file(filepath: str) -> list:
+def parse_pdf_file(file_path: str) -> list:
     """
     Parse the text content of a PDF file and return it as a list of lines.
 
     Args:
-        filepath (str): The path to the PDF file to be parsed.
+        file_path (str): The path to the PDF file to be parsed.
 
     Returns:
         list: A list of lines containing the extracted text content from the PDF file.
     """
-    reader = PdfReader(filepath)
+    reader = PdfReader(file_path)
 
     file_content = []
     for page in reader.pages:
@@ -43,18 +44,18 @@ def parse_pdf_file(filepath: str) -> list:
     return file_content
 
 
-def get_timetable_from_parsed_pdf(station: str, file_content: list) -> str:
+def get_timetable_from_parsed_pdf(train_station: str, file_content: list) -> str:
     """
-    Extract train timetables from the parsed PDF file for a given train station.
+    Extract train timetable from the parsed PDF file for a given train station.
 
     Args:
-        station (str): Name of the station to search for in the timetables.
+        train_station (str): Name of the station to search for in the timetables.
         file_content (list): The text content of the parsed PDF file containing the timetables.
 
     Returns:
         str: A train timetable for a given train station.
     """
-    station = station.upper()
+    train_station = train_station.upper()
     file_lines = file_content
     timetable = ""
     train_line = file_lines[0]
@@ -67,7 +68,7 @@ def get_timetable_from_parsed_pdf(station: str, file_content: list) -> str:
         if "kierunek" in file_line:
             direction = file_line
             timetable += direction + '\n'
-        if station in file_line:
+        if train_station in file_line:
             if "o" in file_line:
                 timetable += file_line + '\n'
             elif "p" in file_line:
@@ -78,35 +79,38 @@ def get_timetable_from_parsed_pdf(station: str, file_content: list) -> str:
     return timetable
 
 
-def save_timetable_to_file(timetable: str, output_filepath: str) -> None:
+def save_timetable_to_file(timetable: str, output_file_path: str) -> None:
     """
     Saves a train timetable to a text file.
 
     Args:
         timetable (str): The text content of the parsed PDF file containing the timetables.
-        output_filepath (str): The path to the output file.
-    """
-    with open(output_filepath, 'a', encoding='utf-8') as file:
-        file.write(timetable)
-    print(f"Rozkład jazdy zapisano w pliku '{output_filepath}'.")
-
-
-def download_train_timetable(line: str) -> None:
-    """
-    Download the train timetable for a specific train line.
-
-    Args:
-        line (str): The train line for which to retrieve the timetable.
+        output_file_path (str): The path to the output file.
 
     Returns:
         None
     """
-    print(f"Szukam rozkładu jazdy dla linii {line}...")
-    url = f'https://www.wtp.waw.pl/rozklady-jazdy/?wtp_md=3&wtp_ln={line}'
-    text = f'Rozkład jazdy linii {line} '  # space at the end to bypass S40 line
+    with open(output_file_path, 'a', encoding='utf-8') as file:
+        file.write(timetable)
+    print(f"Rozkład jazdy zapisano w pliku '{output_file_path}'.")
 
-    logging.debug(f'{url=}')
-    logging.debug(f'{text=}')
+
+def download_train_timetable(train_line: str) -> None:
+    """
+    Download the train timetable for a specific train line.
+
+    Args:
+        train_line (str): The train line for which the timetable will be downloaded.
+
+    Returns:
+        None
+    """
+    print(f"Szukam rozkładu jazdy dla linii {train_line}...")
+    url = f'https://www.wtp.waw.pl/rozklady-jazdy/?wtp_md=3&wtp_ln={train_line}'
+    text = f'Rozkład jazdy linii {train_line} '  # space at the end to bypass S40 line
+
+    logging.debug(f"{url=}")
+    logging.debug(f"{text=}")
 
     links = find_links_on_webpage(
         url=url,
@@ -115,15 +119,14 @@ def download_train_timetable(line: str) -> None:
 
     for link in links:
         file_url = link.get('href')
-        filename = link.text + '.pdf'
+        file_name = link.text + '.pdf'
 
-        downloaded_file_path = os.path.join(downloads_dir, filename)
+        downloaded_file_path = os.path.join(download_dir_path, file_name)
         if not os.path.exists(downloaded_file_path):  # if file not exists in downloads directory
-            download_file(file_url=file_url, downloads_dir=downloads_dir, filename=filename)
+            download_file(file_url=file_url, downloads_dir=download_dir_path, file_name=file_name)
         else:
-            print(f"Plik '{filename}' już istnieje.")
-
-    print(f"Zakończyłem szukanie rozkładu jazdy dla linii {line}.")
+            print(f"Plik '{file_name}' już istnieje.")
+    print(f"Zakończyłem szukanie rozkładu jazdy dla linii {train_line}.")
 
 
 def find_links_on_webpage(url: str, text: str) -> list:
@@ -150,35 +153,35 @@ def find_links_on_webpage(url: str, text: str) -> list:
     raise LinkNotFound
 
 
-def download_file(file_url: str, downloads_dir: str, filename: str) -> None:
+def download_file(file_url: str, downloads_dir: str, file_name: str) -> None:
     """
-    Downloads a file from a given URL and save it to the specified directory with the provided filename.
+    Downloads a file from a given URL and save it to the specified directory with the provided file_name.
 
     Args:
         file_url (str): The URL of the file to download.
         downloads_dir (str): The directory where the file will be saved.
-        filename (str): The name of the file to be saved as.
+        file_name (str): The name of the file to be saved as.
 
     Returns:
         None
     """
-    logging.debug(f'{file_url=}')
+    logging.debug(f"{file_url=}")
 
     response = requests.get(file_url, stream=True)
     if response.status_code == 200:
         file_length = int(response.headers.get('content-length', 0))
-        logging.debug(f'{file_length=}')
+        logging.debug(f"{file_length=}")
         chunk_size = 1024
 
-        download_path = os.path.join(downloads_dir, filename)
-        logging.debug(f'{download_path=}')
+        download_file_path = os.path.join(downloads_dir, file_name)
+        logging.debug(f"{download_file_path=}")
 
-        os.makedirs(os.path.dirname(download_path), exist_ok=True)
+        os.makedirs(os.path.dirname(download_file_path), exist_ok=True)
 
-        print(f"Rozpoczynam pobieranie pliku '{filename}'...")
+        print(f"Rozpoczynam pobieranie pliku '{file_name}'...")
 
-        with open(download_path, 'wb') as file, tqdm(
-                desc=filename,
+        with open(download_file_path, 'wb') as file, tqdm(
+                desc=file_name,
                 total=file_length,
                 unit='iB',
                 unit_scale=True,
@@ -187,42 +190,80 @@ def download_file(file_url: str, downloads_dir: str, filename: str) -> None:
             for data in response.iter_content(chunk_size=chunk_size):
                 size = file.write(data)
                 bar.update(size)
-        print(f"Pobrano plik: {filename}.")
+        print(f"Pobrano plik: {file_name}.")
     else:
-        print(f"Nie udało się pobrać pliku '{filename}' z lokalizacji: '{file_url}'.")
+        print(f"Nie udało się pobrać pliku '{file_name}' z lokalizacji: '{file_url}'.")
     sleep(latency)
 
 
+def load_config_from_file(yaml_path: str) -> tuple:
+    """
+    Loads configuration from a YAML file and assigns it to variables.
+
+    Args:
+        yaml_path (str): Path to the YAML configuration file.
+
+    Returns:
+        tuple: A tuple containing the following elements:
+            - train_lines (list): List of train lines for which the timetable will be downloaded.
+            - train_stations (list): List of train stations for which the timetable will be searched.
+            - download_dir_path (str): Absolute path to the download directory.
+            - output_file_path (str): Path to the output text file.
+            - latency (float): Latency after downloading each file.
+
+    Note:
+        This function reads the specified YAML file to extract relevant configuration
+        information such as train lines, train stations, download directory path,
+        output file path, and latency value. It returns these values as a tuple for further use.
+    """
+    with open('config.yaml', 'r', encoding='utf-8') as file:
+        config = yaml.safe_load(file)
+
+    train_lines = config['train_lines']
+    logging.debug(f"{train_lines=}")
+
+    train_stations = config['train_stations']
+    logging.debug(f"{train_stations=}")
+
+    download_dir_path = os.path.abspath(
+        os.path.join(os.environ.get('HOMEPATH'), 'Desktop', config['download_dir'])
+    )
+    logging.debug(f"{download_dir_path=}")
+
+    output_file_path = os.path.join(download_dir_path, config['output_file_name'])
+    logging.debug(f"{output_file_path=}")
+
+    latency = config['latency']
+    logging.debug(f"{latency=}")
+
+    return train_lines, train_stations, download_dir_path, output_file_path, latency
+
+
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.DEBUG)
+    logging.basicConfig(level=logging.INFO)
     pypdf_logger = logging.getLogger('pypdf')
     pypdf_logger.setLevel(logging.ERROR)  # disable WARNING messages from PyPDF library
-    latency = 0.75
-    downloads_dir = os.path.abspath(
-        os.path.join(os.environ.get('HOMEPATH'), 'Desktop', 'Rozkłady jazdy pociągów')
-    )
 
-    try:
-        download_train_timetable(line='S3')
-        download_train_timetable(line='S4')
-    except requests.exceptions.ConnectionError:
-        print("Błąd połączenia!")
-    except LinkNotFound:
-        print("Nie znaleziono linków zawierających szukany tekst!")
+    train_lines, train_stations, download_dir_path, output_file_path, latency = load_config_from_file('config.yaml')
 
-    for filename in os.listdir(downloads_dir):
-        # logging.debug(f"{filename=}")
+    for train_line in train_lines:
+        try:
+            download_train_timetable(train_line=train_line)
+        except requests.exceptions.ConnectionError:
+            print("Błąd połączenia!")
+        except LinkNotFound:
+            print("Nie znaleziono linków zawierających szukany tekst!")
 
-        if filename.endswith('.pdf'):
-            print(f"Przetwarzam plik: {filename}")
+    for file_name in os.listdir(download_dir_path):
+        if file_name.endswith('.pdf'):
+            print(f"Przetwarzam plik: {file_name}")
 
-            filepath = os.path.join(downloads_dir, filename)
-            with open(filepath, 'r'):
-                pdf_content = parse_pdf_file(filepath=filepath)
+            file_path = os.path.join(download_dir_path, file_name)
+            with open(file_path, 'r'):
+                pdf_content = parse_pdf_file(file_path=file_path)
 
-        train_timetable = get_timetable_from_parsed_pdf(station='wieliszew', file_content=pdf_content)
-        logging.debug(f"{train_timetable=}")
+            for train_station in train_stations:
+                train_timetable = get_timetable_from_parsed_pdf(train_station=train_station, file_content=pdf_content)
+                logging.debug(f"{train_timetable=}")
 
-        save_timetable_to_file(
-            timetable=train_timetable, output_filepath=os.path.join(downloads_dir, 'Rozkład jazdy.txt')
-        )
+                save_timetable_to_file(timetable=train_timetable, output_file_path=output_file_path)
